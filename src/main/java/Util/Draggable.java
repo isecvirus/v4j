d250a6df41d66f0550107f34e6741a106032a2a1
Virus <43806178+isecvirus@users.noreply.github.com>
2024-05-23 21:03:39 +0300

    Add files via upload

--- src/main/java/Util/Draggable.java
+++ src/main/java/Util/Draggable.java
@@ -1,8 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Util;
+package v4j.Util;
 
 import java.awt.Component;
 import java.awt.event.MouseAdapter;
@@ -10,30 +6,14 @@ import java.awt.event.MouseEvent;
 
 /**
  *
- * @author kiwif
+ * @author virus
+ * @version 1.1.1
  */
-public class Dragable {
+public class Draggable {
 
     private int x, y;
-
-    public Dragable(Component comp) {
-        comp.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                x = e.getX();
-                y = e.getY();
-            }
-        });
-
-        comp.addMouseMotionListener(new MouseAdapter() {
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                comp.setLocation(e.getX() - x, e.getY() - y);
-            }
-        });
-    }
     
-    public Dragable(Component comp, Component move) {
+    public Draggable(Component comp) {
         comp.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
@@ -45,7 +25,7 @@ public class Dragable {
         comp.addMouseMotionListener(new MouseAdapter() {
             @Override
             public void mouseDragged(MouseEvent e) {
-                move.setLocation(e.getXOnScreen()- x, e.getYOnScreen()- y);
+                comp.setLocation(e.getXOnScreen()- x, e.getYOnScreen()- y);
             }
         });
     }
